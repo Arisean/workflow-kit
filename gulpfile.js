ac774ff0b0792bfafaa70b9f8b62e68e2f8a8d02
Arisean <arisean.miller@hotmail.com>
2017-07-26 18:10:34 -0500

    Added New Files

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,35 @@
 var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
 var sass        = require('gulp-sass');
+var jade        = require('gulp-pug');
+var sequirence  = require('run-sequence');
+var colors      = require('colors');
 
-// Static Server + watching scss/html files
-gulp.task('serve', ['sass'], function() {
+
+gulp.task('build', ['sass','jade'], function() {
 
     browserSync.init({
-        server: "./app"
+      server: 'app'
     });
 
-    gulp.watch("app/scss/*.scss", ['sass']);
-    gulp.watch("app/*.html").on('change', browserSync.reload);
+
+    gulp.watch("app/sass/**/*.sass", ['sass']);
+    gulp.watch("app/jade/**/*.pug", ['jade']);
+    gulp.watch("app/jade/**/*.jade", ['jade']);
+    gulp.watch("app/jade/*.html").on('change', browserSync.reload);
 });
 
-// Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function() {
-    return gulp.src("app/scss/*.scss")
-        .pipe(sass())
-        .pipe(gulp.dest("app/css"))
-        .pipe(browserSync.stream());
+  return gulp.src("app/sass/*.sass")
+    .pipe(sass())
+    .pipe(gulp.dest('app/css'))
+    .pipe(browserSync.stream());
 });
 
-gulp.task('default', ['serve']);
+gulp.task('jade', function() {
+  return gulp.src("app/jade/*.pug")
+    .pipe(jade())
+    .pipe(gulp.dest('app'))
+    .pipe(browserSync.stream());
+});
+gulp.task('default', ['build']);
